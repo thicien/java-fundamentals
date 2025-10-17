7f4e9716f74cbfb2b6e0072a54fcf514bf1038b2
mugisha thicien <mugishathicien04@gmail.com>
2025-10-17 14:06:58 +0200

    natural numbers added

---
+++ src/FirstTenNatural.java
@@ -0,0 +1,9 @@
+public class FirstTenNatural {
+    public static void main(String[] args) {
+        System.out.println("First 10 natural numbers:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
